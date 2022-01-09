6791b6f65cadb8e1d962bfbb0a3b868383384480
Julia Tan <juliajtan@gmail.com>
2022-01-09 15:38:12 +0000

    Set up Infura and Metamask wallet mnemonic

--- README.md
+++ README.md
@@ -16,6 +16,8 @@
 
 - `npm run test` - run test suite
 - `node deploy.js` - deploy contract to test network
+  - You'll need to create a `.env` file with your `INFURA_API` and Metamask wallet `MNEMONIC`. This is parsed in `compile.js`, so that the variables are available when running `node deploy.js`.
+  - If you get an error to do with `ERR_OSSL_EVP_UNSUPPORTED`, it'll be due to an issue with the node version you're using. I use `nvm` to switch to version 15.8.0, which does the trick.
 
 ## Resources
 
--- compile.js
+++ compile.js
@@ -1,8 +1,12 @@
-const path = require("path");
-const fs = require("fs");
-const solc = require("solc");
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
-const lotteryPath = path.resolve(__dirname, "contracts", "Lottery.sol");
-const source = fs.readFileSync(lotteryPath, "utf8");
+const path = require('path')
+const fs = require('fs')
+const solc = require('solc')
 
-module.exports = solc.compile(source, 1).contracts[":Lottery"];
+const lotteryPath = path.resolve(__dirname, 'contracts', 'Lottery.sol')
+const source = fs.readFileSync(lotteryPath, 'utf8')
+
+module.exports = solc.compile(source, 1).contracts[':Lottery']
